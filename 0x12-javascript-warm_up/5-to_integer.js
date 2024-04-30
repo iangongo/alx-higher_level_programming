e957228aee9de897fd4ae80026c7d1eeff20d29e
iangongo <ianmwanyaegongo@gmail.com>
2024-04-29 18:28:04 -0700

    more  bug fixes task 05

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-// prints two arguments passed to it, in the following format: “ is ”
-
-if (isNaN(process.argv[2])) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + parseInt(process.argv[2]));
-}
+let n = parseInt(process.argv[2]);
+if (isNaN(n)) console.log('Not a number');
+else console.log('My number: ' + n);
